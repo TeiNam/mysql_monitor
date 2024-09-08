732166a2ab2ef51b0ec2e67ab702120567f6bac5
teinam <teinam@rastalion.dev>
2024-09-08 23:09:09 +0900

    Third commit

--- asgi.py
+++ asgi.py
@@ -1,3 +1,13 @@
 from apis import app as application
+import uvicorn
+from configs.app_conf import app_settings
 
 __all__ = ['application']
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "asgi:application",
+        host=app_settings.HOST,
+        port=app_settings.PORT,
+        reload=True
+    )

56cbd44f5d7583c053c3d0c1e26df971a3962dcc
TeiNam <teinam@rastalion.dev>
2024-09-10 17:10:31 +0900

    다른 수집기 마저 추가

--- run.sh
+++ run.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
-# Activate virtual environment if you're using one
-# source /path/to/your/venv/bin/activate
+# Check if pyenv is available
+if ! command -v pyenv &> /dev/null
+then
+    echo "pyenv could not be found. Please make sure it's installed and initialized."
+    exit 1
+fi
+
+# Activate pyenv and the virtual environment
+echo "Activating virtual environment: mysql_monitor"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+pyenv activate mysql_monitor
+
+# Check if virtual environment activation was successful
+if [[ "$VIRTUAL_ENV" != *"mysql_monitor"* ]]; then
+    echo "Failed to activate the virtual environment."
+    exit 1
+fi
 
 # Set environment variables if needed
 # export ENVIRONMENT=production
 
 # Run the application using uvicorn
+echo "Starting the application..."
 uvicorn asgi:app --host 0.0.0.0 --port 8000 --workers 4 --reload
+
+# Deactivate the virtual environment
+echo "Deactivating virtual environment"
+pyenv deactivate
+
+echo "Script completed."

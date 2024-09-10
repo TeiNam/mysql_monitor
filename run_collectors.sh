5c5aed04d840fc95297299ab47ac99edffccd47b
TeiNam <teinam@rastalion.dev>
2024-09-10 19:29:13 +0900

    존나게 수정

--- run_collectors.sh
+++ run_collectors.sh
@@ -25,26 +25,9 @@ fi
 # Run the collectors
 echo "Starting the collectors..."
 
-# MySQL Slow Queries Collector
-python collectors/mysql_slow_queries.py &
-SLOW_QUERIES_PID=$!
+python collectors.py
 
-# MySQL Command Status Collector
-python collectors/mysql_command_status.py &
-COMMAND_STATUS_PID=$!
-
-# MySQL Disk Status Collector
-python collectors/mysql_disk_status.py &
-DISK_STATUS_PID=$!
-
-# RDS Instance Status Collector
-python collectors/rds_instance_status.py &
-RDS_STATUS_PID=$!
-
-# Wait for all background processes to complete
-wait $SLOW_QUERIES_PID $COMMAND_STATUS_PID $DISK_STATUS_PID $RDS_STATUS_PID
-
-echo "All collectors have finished."
+echo "Collectors have finished."
 
 # Deactivate the virtual environment
 echo "Deactivating virtual environment"
